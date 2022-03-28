8b6bbd11821fe48fa16a661879c5285043ca6018
Emanuel-san <emanuel.sanchez@ha.ax>
2022-03-28 17:29:42 +0300

    implemented fn random_suit for Card

--- src/card.rs
+++ src/card.rs
@@ -1,8 +1,9 @@
-//Should i separate Card object from Deck object in different files?
+use std::num::ParseIntError;
+
 #[derive(Debug)]
 pub struct Card{
-    value: u8,
-    suit: CardSuit,
+    pub value: u8,
+    pub suit: CardSuit,
 }
 #[derive(Debug)]
 pub enum CardSuit{
@@ -13,10 +14,21 @@ pub enum CardSuit{
 }
 
 impl Card {
+
     pub fn new(value: u8, suit: CardSuit) -> Self {
         Self {
             value,
             suit
+        }
+    }
+
+    pub fn random_suit(rng_value: u8) -> CardSuit{
+        match rng_value {
+            1 => CardSuit::Diamond,
+            2 => CardSuit::Clove,
+            3 => CardSuit::Spade,
+            4 => CardSuit::Heart,
+            _ => panic!("Card:: Failed choosing random suit")
         }
     }
 }
