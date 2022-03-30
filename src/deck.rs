49bf82daf76bf76cf662b82856ca530e20843929
Emanuel-san <emanuel.sanchez@ha.ax>
2022-03-30 09:17:07 +0300

    Added modules to main and wrote some test code for Hand.

--- src/deck.rs
+++
@@ -1,6 +0,0 @@
-mod card;
-use card::*;
-
-pub struct Deck {
-    deck_vec: Vec<Card>
-}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+mod card;
+mod hand;
+use card::*;
+use hand::*;
+
+//use crate::card::CardSuit;
+//use crate::CardSuit::*;
+//use rand::prelude::*;
 fn main() {
+
+    // let five_card_hand = Hand::new();
+    // println!("{:?}", five_card_hand);
+    // let Hand{ 
+    //     hand_vec: decon_hand_vec
+    //     } = five_card_hand;
+    //Hand::chk_duplicate(&decon_hand_vec)
+
+
     
 }
