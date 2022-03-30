11e49e4c81330a010edf55228f2d2c8d170e3fbf
Emanuel-san <emanuel.sanchez@ha.ax>
2022-03-30 12:11:46 +0300

    move push card logic til seperate fn push_card_to_hand

--- src/hand.rs
+++ src/hand.rs
@@ -7,17 +7,13 @@ pub struct Hand {
 }
 
 impl Hand {
-    
+
     /*Constructor */
     pub fn new() -> Self {
         let mut hand_of_cards = Vec::new();
-        let mut new_card: Card;
         
         while hand_of_cards.len() < 5 { //loop untill vec holds 5 elements
-            new_card = Card::get_card(); //get a card
-            if Hand::chk_duplicate(&hand_of_cards, &new_card) == false{ //check if the card already exist in a vec
-                hand_of_cards.push(new_card); //push to vec if its not a duplicate
-            }
+            Hand::push_card_to_hand(&mut hand_of_cards);
         }
 
         Self {
@@ -26,13 +22,19 @@ impl Hand {
     }
 
     pub fn chk_duplicate(vec_of_cards: &Vec<Card>, new_card: &Card) -> bool{
-
         for card in vec_of_cards{
-            if card.value == new_card.value && card.suit == new_card.suit {
+            if card.value == new_card.value && card.suit == new_card.suit { // check if value and suit is the same
                 return true
             }
         }
         return false
+    }
+
+    pub fn push_card_to_hand(vec_of_cards: &mut Vec<Card>){
+        let new_card = Card::new();
+        if Hand::chk_duplicate(&vec_of_cards, &new_card) == false{ //check if the card already exist in a vec
+            vec_of_cards.push(new_card); //push to vec if its not a duplicate
+        }   
     }
 }
 
