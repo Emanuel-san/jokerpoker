0940f3b92b3081899ccdcf26febaf24946e93a61
Emanuel-san <emanuel.sanchez@ha.ax>
2022-03-31 15:47:19 +0300

    added fn generate_five_card_hand and print_hand

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,47 @@ mod hand;
 //use card::*;
 use hand::*;
 
+use crate::card::{Card, CardSuit};
+
 //use crate::card::CardSuit;
 //use crate::CardSuit::*;
 //use rand::prelude::*;
 
-fn generate_five_card_hand(empty_hand: &mut Hand){
-    while empty_hand.hand_vec.len() < 5 { //loop untill vec holds 5 elements
-        empty_hand.push_card_to_hand();
+fn generate_five_card_hand(hand_obj: &mut Hand){
+    while hand_obj.hand_vec.len() < 5 { //loop untill vec holds 5 elements
+        hand_obj.draw_card_to_hand();
     }
 }
+
+fn print_hand(hand_obj: &Hand) {
+    let Hand{
+        hand_vec: temp_hand
+    } = hand_obj;
+    for card in temp_hand{
+        print!("{} of ", card.value);
+        match card.suit {
+            CardSuit::Diamond => print!("Diamonds | "),
+            CardSuit::Spade => print!("Spades | "),
+            CardSuit::Clove => print!("Cloves | "),
+            CardSuit::Heart => print!("Hearts | "),
+        }
+    }
+}
+
+
 fn main() {
     let mut five_card_hand = Hand::new();
+    let mut discarded_cards: Vec<Card> = Vec::new();
     generate_five_card_hand(&mut five_card_hand);
+    //five_card_hand.sort_hand_by_value();
+    //println!("Five card hand: {:?}", five_card_hand);
 
-    println!("{:?}", five_card_hand);
-    // let Hand{ 
-    //     hand_vec: deconstructed_hand
-    //     } = five_card_hand;
-
-
-    
+    let card = five_card_hand.hand_vec.remove(1);
+    discarded_cards.push(card);
+    discarded_cards.push(five_card_hand.discard_card_from_hand(3));
+    //five_card_hand.discard_card_from_hand(3);
+    //println!("Five card hand: {:?}", five_card_hand.hand_vec);
+    //println!("Discarded: {:?}", discarded_cards);
+    generate_five_card_hand(&mut five_card_hand);
+    print_hand(&five_card_hand);
 }
