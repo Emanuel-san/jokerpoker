67228c00c4d25bc00409e8a1c05b1b7776985f92
Emanuel-san <emanuel.sanchez@ha.ax>
2022-04-03 03:35:57 +0300

    added algorithm to evaluate a straight in fn evaluate_hand

--- src/main.rs
+++ src/main.rs
@@ -27,26 +27,66 @@ fn print_hand(hand_obj: &Hand) {
             CardSuit::Heart => print!("Hearts | "),
         }
     }
+    println!("\n");
 }
 
 fn evaluate_hand(poker_hand: &Hand){
-    let mut suit_counter = vec![0u8; 4];
-    let mut value_counter = vec![0u8; 15];
+    let mut suit_tracker = vec![0u8; 4];
+    let mut value_tracker = vec![0u8; 15];
     let mut jokers: u8 = 0;
 
     for card in &poker_hand.hand_vec{
-        value_counter[card.value as usize] += 1;
+        value_tracker[card.value as usize] += 1;
         match card.suit {
-            CardSuit::Diamond => suit_counter[0] += 1,
-            CardSuit::Clove => suit_counter[1] += 1,
-            CardSuit::Spade => suit_counter[2] += 1,
-            CardSuit::Heart => suit_counter[3] += 1,
+            CardSuit::Diamond => suit_tracker[0] += 1,
+            CardSuit::Clove => suit_tracker[1] += 1,
+            CardSuit::Spade => suit_tracker[2] += 1,
+            CardSuit::Heart => suit_tracker[3] += 1,
             CardSuit::Joker => jokers +=1
         }
     }
-    // println!("Values: {:?}", value_counter);
+
+    value_tracker[13] = value_tracker[0]; //We need to add aces to the highest value count aswell since we only count them as lowest when checking values in previous for loop.
+    //println!("Values: {:?}", value_counter);
     // println!("Suits: {:?}", suit_counter);
     // println!("Jokers: {}", jokers);
+
+    //filter out the suit_tracker where 0 values occur and collect the none zero values into a new vec (counted_suits)...
+    let mut counted_suits = suit_tracker.into_iter()
+    .filter(|&x| x > 0).collect::<Vec<_>>();
+    counted_suits.sort_unstable();//... and sort it.
+    counted_suits[0] += jokers; // add jokers to the index 0 incase they create a flush. (i.e index == 3 and we have 2 jokers, then we have a flush.)
+    //println!("{:?}", counted_suits);
+    let is_flush = counted_suits[0] == 5; // If index 0 contains element 5 then we have a flush.
+    let mut is_straight = false;
+
+    let mut vec_pointer = 14;
+    //We use a "pointer" to avoid looking at cards again that was a part of another sequence.
+    //This is only for checking if we have a poker hand with a straight.
+
+    while vec_pointer > 3{// If the pointer reachs value 3 its no point at looking at the rest since Ace-2-3-4 will not make a straight. 
+        let mut jokers_left = jokers; //Each time a sequence "fails" we will shadow this variable and re-declare jokers to use in the next sequence
+        let mut straight_cards = 0; // Same as above though we reset to 0 on "failed" sequence.
+        for i in (0..vec_pointer).rev(){ // represents the sequence (0..ptr) = 0-13
+            println!("i: {} | ptr: {} | jokers: {} | straight_cards: {}| faces[i]: {}",i, vec_pointer, jokers_left, straight_cards, value_tracker[i]);
+            if value_tracker[i] == 0 { // if element at index i is 0...
+                if jokers_left == 0 { // ...then we check if we have jokers left to use.
+                    break; // If not then we break out.
+                }
+                jokers_left -= 1; //Remove joker is used
+            }
+            else if i==vec_pointer-1 { //Since we use jokers we will only reduce vec_pointer if this is true, else its possible we miss a possible straight in the next sequence
+                vec_pointer -= 1;
+            }
+            straight_cards += 1; //if we found a index with a none zero value or we used a joker, add 1 straight card.
+        }
+        vec_pointer -= 1; //always reduce by atleast one since even tho we used jokers we can guarantee the first index we check will not form a straight.
+        if straight_cards == 5 { // We have a straight!
+            is_straight = true;
+            break;
+        }
+    }
+
 }
 
 
