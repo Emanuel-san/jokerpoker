2df056b443961c7a3e2cac87b461a78fc0215f33
Emanuel-san <emanuel.sanchez@ha.ax>
2022-04-22 11:08:17 +0300

    Rebuilt deck to work with bevy ECS

--- src/deck.rs
+++ src/deck.rs
@@ -1,12 +1,42 @@
 use crate::card::*;
+use bevy::prelude::*;
 use rand::prelude::*;
 
-#[derive(Debug)]
-pub struct Deck{
+pub struct DeckPlugin;
+
+#[derive(Debug, Component)]
+pub struct DeckState{
     pub deck_vec: Vec<Card>,
 }
 
-impl Deck {
+impl Plugin for DeckPlugin {
+    fn build(&self, app: &mut App){
+        let deck_res = DeckState {
+            deck_vec: Vec::new()
+        };
+        app.insert_resource(deck_res)
+            .add_startup_system(get_deck);
+
+    }
+}
+
+/// Generates 52 cards into a deck with 2 jokers
+/// # Panics
+/// Function panics if the vector length its trying to return is not 54
+fn get_deck(mut deck_init: ResMut<DeckState>) {
+    let mut rng = thread_rng();
+    for suit in 0..=3{ //Generate 4 suits
+        for card_face in 0..=12{ //13 card faces for each suit (Ace-King)
+            deck_init.deck_vec.push(Card::get_card(card_face, suit));
+        }
+    }
+    deck_init.deck_vec.push(Card::get_card(14, -1)); //Allow 2 Jokers
+    deck_init.deck_vec.push(Card::get_card(14, -1));
+    assert_eq!(deck_init.deck_vec.len(), 54, "Deck::generate_deck Vector length has to equal 54, current length is {}", deck_init.deck_vec.len()); //Make sure we have 54 cards (52 + two Jokers)
+    deck_init.deck_vec.shuffle(&mut rng); //shuffle deck
+}
+
+impl DeckState {
 
     ///Deck constructor
     fn new() -> Self{
@@ -15,23 +45,6 @@ impl Deck {
         Self{
             deck_vec: deck_of_cards,
         }
-    }
-    /// Generates 52 cards into a deck with 2 jokers
-    /// # Panics
-    /// Function panics if the vector length its trying to return is not 54
-    pub fn get_deck() -> Deck {
-        let mut rng = thread_rng();
-        let mut deck_init = Deck::new();
-        for suit in 0..=3{ //Generate 4 suits
-            for card_face in 0..=12{ //13 card faces for each suit (Ace-King)
-                deck_init.deck_vec.push(Card::get_card(card_face, suit));
-            }
-        }
-        deck_init.deck_vec.push(Card::get_card(14, -1)); //Allow 2 Jokers
-        deck_init.deck_vec.push(Card::get_card(14, -1));
-        assert_eq!(deck_init.deck_vec.len(), 54, "Deck::generate_deck Vector length has to equal 54, current length is {}", deck_init.deck_vec.len()); //Make sure we have 54 cards (52 + two Jokers)
-        deck_init.deck_vec.shuffle(&mut rng); //shuffle deck
-        deck_init
     }
 }
 
