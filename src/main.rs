48ecf81a5073698b1c51a9e860b1af27cb93ecfb
Emanuel-san <emanuel.sanchez@ha.ax>
2022-04-27 22:11:08 +0300

    game loop with card select

--- src/main.rs
+++ src/main.rs
@@ -19,23 +19,32 @@ use card::*;
 
 
 fn main() {
-    let mut deck_of_cards = Deck::get_deck();
-    let mut five_card_hand = Hand::new();
-    let mut holder: Vec<CharHolder> = Vec::new();
-    five_card_hand.draw_until_five_cards(&mut deck_of_cards);
-    holder = format_hand(&five_card_hand);
-    
-    print_hand(&holder);
 
-    five_card_hand.discard_card_from_hand(0);
-    five_card_hand.draw_until_five_cards(&mut deck_of_cards);
-    five_card_hand.hand_vec[1].selected = true;
-    holder = format_hand(&five_card_hand);
-    print_hand(&holder);
-    //println!("{:?}", five_card_hand);
+    let state = MachineState::Playing;
 
-    println!("{}", evaluate_hand(&five_card_hand));
-    //let mut input = UserInput::get_user_input().parse_input_to_digits();
-
-
+    while state == MachineState::Playing {
+        let mut deck_of_cards = Deck::get_deck();
+        let mut five_card_hand = Hand::new();
+        five_card_hand.draw_until_five_cards(&mut deck_of_cards);
+        let mut holder = format_hand(&five_card_hand);
+        print_hand(&holder);
+        
+        loop {
+            let mut input = UserInput::get_user_input();
+            if input.input_string.trim().to_lowercase() == "draw"{
+                break;
+            } else {
+                if let Ok(parsed_input) = input.parse_and_chk_select_input(){
+                    let mut card: &mut Card = &mut five_card_hand.hand_vec[parsed_input - 1];
+                    if card.selected == true {
+                        card.selected = false;
+                    } else {
+                        card.selected = true;
+                    }
+                }
+            }
+            holder = format_hand(&five_card_hand);
+            print_hand(&holder);
+        }
+    }
 }
