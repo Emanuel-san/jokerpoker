5f6a8681c9f83500b9bdbc3d6e1321f26905ebf9
Emanuel-san <emanuel.sanchez@ha.ax>
2022-05-12 00:16:31 +0300

    added a debug_hand

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,12 @@ fn main() {
             current_funds.reduce_funds();
             let mut deck_of_cards = Deck::get_deck();
             let mut five_card_hand = Hand::new();
+            let mut debug_hand = Hand::new();
+            debug_hand.test_hand();
             //five_card_hand.draw_card(&mut deck_of_cards);
             five_card_hand.draw_until_five_cards(&mut deck_of_cards);
             let mut holder = format_hand(&five_card_hand);
             print_hand_and_credits(&holder, &current_funds);
-            println!("{:?}", five_card_hand);
             state = MachineState::CardSelection;
 
             while state == MachineState::CardSelection {
@@ -48,7 +49,6 @@ fn main() {
             print_hand_and_credits(&holder, &current_funds);
             let evaluation = evaluate_hand(&five_card_hand);
             evaluation.chk_evaluation_for_win(&mut state);
-            println!("{:?}", five_card_hand);
             let mut credits_won = evaluation.hand_value;
 
             while state == MachineState::Win {
@@ -59,20 +59,21 @@ fn main() {
                 while state == MachineState::Double {
                     let mut doubling_deck = Deck::get_deck();
                     let mut doubling_hand = Hand::new();
-                    let mut input_control = Control::Rejected;
+                    let mut input_control = InputControl::Invalid;
                     let mut selected_index: usize = 0;
                     doubling_hand.draw_card(&mut doubling_deck);
                     holder = format_hand(&doubling_hand);
                     print_hand_and_credits(&holder, &current_funds);
                     println!("  DEALERS CARD\n");
-                    while input_control == Control::Rejected{
+                    while input_control == InputControl::Invalid{
                         player_input = UserInput::get_user_input();
-                        selected_index = player_input.double_input(&mut input_control) - 1;
+                        selected_index = player_input.double_input(&mut input_control);
                     }
                     doubling_hand.draw_until_five_cards(&mut doubling_deck);
                     doubling_hand.hand_vec[selected_index].alter_selection();
                     holder = format_hand(&doubling_hand);
                     print_hand_and_credits(&holder, &current_funds);
+                    println!("  DEALERS CARD\n");
                     evaluate_doubling(&doubling_hand, &mut credits_won, &selected_index, &mut state);
                 }
             }
